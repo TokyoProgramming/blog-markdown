1aa5d824fb58ab42bcddac176b73132b245c1a80
TokyoProgramming <samuraidjango.js@gmail.com>
2022-01-06 21:32:08 +0900

    cached

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,6 +6,7 @@ export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_EN === 'product') {
+    posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
     posts = files.map((filename) => {
